86f81b3d8b13ae7e7c822df0c07b45c3cac64240
Tanapol Kaewpicahi <88494547+TanapolKaewpichai@users.noreply.github.com>
2022-03-18 17:16:25 +0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let readlineSynce = require("readline-sync");
-
-let name = readlineSynce.question("What is your name?");
-
-if (name.includes("!")) {
-  console.log(`HELLO ${name}. WHY ARE WE SCREAMING?`);
-} else {
-  console.log(`Hello ${name}`);
-}
